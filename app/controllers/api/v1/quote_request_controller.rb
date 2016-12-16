8d828e126a0025caab3018cabb67b90edfe6098a
Om Patel <om@logistixian.com>
2016-12-16 19:00:32 +0530

    Date accepted in quote request

--- app/controllers/api/v1/quote_request_controller.rb
+++ app/controllers/api/v1/quote_request_controller.rb
@@ -2,23 +2,25 @@ require 'json'
 class Api::V1::QuoteRequestController < ApplicationController
 
   def create_quote_request
-    p params
     quote_request = QuoteRequest.create()
+    quote_request.transit_type = params[:transitType]
+    quote_request.requester = params[:client]
     quote_request.port_of_loading = {:code => params[:fromCode], :name => "abc"}
     quote_request.port_of_discharge = {:code => params[:toCode], :name => "def"}
-    quote_request.transit_type = params[:transitType]
-    quote_request.shipment_date = "2016-12-20"
+    quote_request.shipment_ready_date = Date.strptime(params[:startDate], '%m/%d/%Y')
+    quote_request.temp_controlled = params[:tempControlled]
+    quote_request.hazardous = params[:hazardous]
+    quote_request.air_packages = params[:airPackages]
+    quote_request.commodity = params[:product]
     quote_request.save!
     render json: {quote_request_id: quote_request.id}
   end
 
   def fetch_air_schedules
     quote_request = QuoteRequest.find(params[:quote_request_id])
-    p quote_request
-    options = {:fromCode => quote_request.port_of_loading["code"], :toCode => quote_request.port_of_discharge["code"], :date => quote_request.shipment_date }
+    options = {:fromCode => quote_request.port_of_loading["code"], :toCode => quote_request.port_of_discharge["code"], :date => quote_request.shipment_ready_date.strftime('%F')}
     schedules_json = WinService.fetch_schedules(options)
     win_schedules = JSON.parse(schedules_json)
-    p win_schedules
     render json: win_schedules.map {|schedule| Api::V1::WinSchedule.new(schedule)}
   end
 
--- db/migrate/20161208102236_create_quote_requests.rb
+++ db/migrate/20161208102236_create_quote_requests.rb
@@ -3,18 +3,17 @@ class CreateQuoteRequests < ActiveRecord::Migration[5.0]
     enable_extension :hstore
     create_table :quote_requests do |t|
       t.string :transit_type
-      t.string :shipper
+      t.string :requester
+      t.string :commodity
       t.hstore :port_of_loading
       t.hstore :port_of_discharge
-      t.string :to_city
-      t.string :from_city
       t.string :to_address
       t.string :from_address
-      t.json :packages # package = [{l,b,h,w,vw},...]
+      t.json :air_packages # package = [{l,b,h,w,vw},...]
       t.decimal :shipment_valuation
-      t.boolean :hazardous?
-      t.boolean :insurance?
-      t.boolean :temp_controlled?
+      t.boolean :hazardous
+      t.boolean :insurance
+      t.boolean :temp_controlled
       t.integer :min_temperature
       t.integer :max_temperature
       t.hstore :other_services_charges
